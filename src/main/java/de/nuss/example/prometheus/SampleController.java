b9f651b59a8cc4d21f245c9e7f9a2ed961773d74
Thorsten Nuss <thorsten.nuss@bilfinger.com>
2019-01-08 08:46:40 +0100

    added more examples in sample controller

--- src/main/java/de/nuss/example/prometheus/SampleController.java
+++ src/main/java/de/nuss/example/prometheus/SampleController.java
@@ -1,6 +1,7 @@
 package de.nuss.example.prometheus;
 
 import java.time.ZonedDateTime;
+import java.util.Random;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,51 @@ public class SampleController {
 		metricLogAllCounter.increment();
 
 		return String.format("logs written at %s", ZonedDateTime.now());
+	}
+
+	@GetMapping(path = "memory", produces = "text/plain")
+	public String memory() {
+		long freeMemory = Runtime.getRuntime().freeMemory();
+		long maxMemory = Runtime.getRuntime().maxMemory();
+		long totalMemory = Runtime.getRuntime().totalMemory();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("Memory:\r\n");
+		sb.append("---------------------------------\r\n");
+		sb.append("free: ").append(freeMemory / 1024 / 1024).append("mb\r\n");
+		sb.append("max: ").append(maxMemory / 1024 / 1024).append("mb\r\n");
+		sb.append("total: ").append(totalMemory / 1024 / 1024).append("mb\r\n");
+
+		return sb.toString();
+	}
+
+	@GetMapping(path = "thread", produces = "text/plain")
+	public String startThread() {
+		log.info("start thread");
+		new Thread(() -> {
+			try {
+				log.info("thread started");
+				Thread.sleep(60_000);
+				log.info("thread finished");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+		return String.format("thread started at %s", ZonedDateTime.now());
+	}
+
+	@GetMapping(path = "responseTime", produces = "text/plain")
+	public String responseTime() throws InterruptedException {
+		long begin = System.currentTimeMillis();
+
+		int sleepTime = new Random().nextInt(300) * 10;
+		log.info("sleep {}ms", sleepTime);
+
+		Thread.sleep(sleepTime);
+
+		long end = System.currentTimeMillis();
+
+		return String.format("site generated in %s ms", (end - begin));
 	}
 }
